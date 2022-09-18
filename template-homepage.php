cedac1cbd169d5f33db4dc37ea38dea084ed33cb
Thomas Mol <email@thomasmol.com>
2022-09-18 13:21:31 +0200

    added custom artist booking text

--- template-homepage.php
+++ template-homepage.php
@@ -28,11 +28,12 @@ get_header();
                 <a id="artist" href="artists/<?php global $post;
                                                 echo $post->post_name; ?>" class="group h-80 overflow-hidden border border-transparent grayscale transition duration-200 ease-in-out hover:border hover:border-zinc-500 hover:grayscale-0 md:h-96">
                     <h2 class="absolute px-6 py-4 text-lg font-semibold uppercase text-white">
-                        <?php echo the_field('name'); ?>
+                        <?php the_field('name'); ?>
                     </h2>
                     <div class="hidden group-hover:block absolute bottom-0 text-white px-6 py-4 font-semibold">
                         <?php if (get_field('management')) : ?> <h3 class="uppercase">Management</h3><?php endif; ?>
                         <?php if (get_field('worldwide_booking')) : ?> <h3 class="uppercase">Worldwide booking</h3><?php endif; ?>
+                        <?php if (get_field('custom_booking_text')) : ?> <h3 class="uppercase"><?php the_field('custom_booking_text'); ?></h3><?php endif; ?>
                     </div>
                     <img src="<?php echo the_field('profile_picture'); ?>" alt="" class="h-full w-full object-cover" />
                 </a>
