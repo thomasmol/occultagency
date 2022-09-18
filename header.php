9e3870dcdbd0dd928acae8b6eda1bbf82a9a2e48
Thomas Mol <email@thomasmol.com>
2022-09-18 13:50:20 +0200

    link tree icon

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="nl">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -22,12 +23,18 @@
 </head>
 
 <body>
-<nav>
-	<div class="container mx-auto px-2 md:px-4 text-center flex flex-col">
-		<img src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" class="mx-auto mt-12 mb-6" alt="occult agency logo" width="60" />
-		<a href="/" class="text-black mx-auto text-3xl sm:text-5xl font-sans font-semibold">occult.agency</a>
-        <div class="text-right">
-            <a href="/about" class="hover:underline">contact &amp; about</a>
+    <nav>
+        <div class="container mx-auto px-2 md:px-4 text-center flex flex-col">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" class="mx-auto mt-12 mb-6" alt="occult agency logo" width="60" />
+            <a href="/" class="text-black mx-auto text-3xl sm:text-5xl font-sans font-semibold">occult.agency</a>
+            <div class="text-right">
+                <a href="/about" class="hover:underline">contact &amp; about</a>
+            </div>
         </div>
-	</div>
-</nav>
+    </nav>
+
+    <div class="bottom-5 right-5 fixed">
+        <a href="https://linktr.ee/occultagency" target="_blank" class="p-3 block">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/linktree.svg" alt="linktree" class="h-6 w-6">
+        </a>
+    </div>
