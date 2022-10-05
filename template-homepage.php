99848586b0a210becd7d9a993edab4daa24464ca
Thomas Mol <email@thomasmol.com>
2022-10-05 14:54:16 +0200

    added header text

--- template-homepage.php
+++ template-homepage.php
@@ -31,7 +31,7 @@ get_header();
                         <?php the_field('name'); ?>
                     </h2>
                     <div class="hidden group-hover:block absolute bottom-0 text-white px-6 py-4 font-semibold">
-                        <?php if (get_field('management')) : ?> <h3 class="uppercase">Management</h3><?php endif; ?>
+                        <?php if (get_field('management_toggle')) : ?> <h3 class="uppercase">Management</h3><?php endif; ?>
                         <?php if (get_field('worldwide_booking')) : ?> <h3 class="uppercase">Worldwide booking</h3><?php endif; ?>
                         <?php if (get_field('custom_booking_text')) : ?> <h3 class="uppercase"><?php the_field('custom_booking_text'); ?></h3><?php endif; ?>
                     </div>
